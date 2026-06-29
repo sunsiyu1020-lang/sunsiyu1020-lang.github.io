865416d6a4c2a19d8163168c21e220ced8fc8948
孙思宇 <sunsiyu1020@gmail.com>
2026-06-29 12:20:20 +0800

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,39 +1,76 @@
-particlesJS("particles-js",{
-particles:{
-number:{
-value:80
-},
-color:{
-value:"#00d4ff"
-},
-shape:{
-type:"circle"
-},
-opacity:{
-value:0.5
-},
-size:{
-value:3
-},
-line_linked:{
-enable:true,
-distance:150,
-color:"#00d4ff",
-opacity:0.4,
-width:1
-},
-move:{
-enable:true,
-speed:2
-}
-},
-interactivity:{
-events:{
-onhover:{
-enable:true,
-mode:"repulse"
-}
-}
-},
-retina_detect:true
-});
+particlesJS(
+    "particles-js",
+    {
+        particles: {
+            number: {
+                value: 100
+            },
+
+            color: {
+                value:
+                    "#00d4ff"
+            },
+
+            shape: {
+                type:
+                    "circle"
+            },
+
+            opacity: {
+                value: 0.5
+            },
+
+            size: {
+                value: 3
+            },
+
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color:
+                    "#00d4ff",
+                opacity: 0.4,
+                width: 1
+            },
+
+            move: {
+                enable: true,
+                speed: 2
+            }
+        },
+
+        interactivity: {
+
+            detect_on:
+                "canvas",
+
+            events: {
+
+                onhover: {
+                    enable: true,
+                    mode:
+                        "repulse"
+                },
+
+                onclick: {
+                    enable: true,
+                    mode:
+                        "push"
+                }
+            },
+
+            modes: {
+
+                repulse: {
+                    distance: 120
+                },
+
+                push: {
+                    particles_nb: 4
+                }
+            }
+        },
+
+        retina_detect: true
+    }
+);
