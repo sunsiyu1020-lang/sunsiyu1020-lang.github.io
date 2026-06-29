7851834f78ee17494ac2bdd1e1844d5bc0a7913e
孙思宇 <sunsiyu1020@gmail.com>
2026-06-29 11:27:11 +0800

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,2 +1,52 @@
-JavaScript
-console.log("particles");
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 80
+        },
+        color: {
+            value: "#00d9ff"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.5
+        },
+        size: {
+            value: 3
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#00d9ff",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
+            }
+        },
+        modes: {
+            repulse: {
+                distance: 120
+            },
+            push: {
+                particles_nb: 4
+            }
+        }
+    },
+    retina_detect: true
+});
